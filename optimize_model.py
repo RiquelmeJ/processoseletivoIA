51346cc7f8530ccec2155e70129d04a4aaccb61c
Riquelme Jatay <riquelmejatay@gmail.com>
2026-05-04 21:59:23 -0300

    Versão final

--- optimize_model.py
+++ optimize_model.py
@@ -1,4 +1,32 @@
 import tensorflow as tf
 import os
 
-#insira seu código aqui
+def main():
+    model_path = "model.h5"
+    tflite_model_path = "model.tflite"
+
+    if not os.path.exists(model_path):
+        print(f"Erro: sem arquivo {model_path}.")
+        return
+
+  
+    model = tf.keras.models.load_model(model_path)
+
+
+    converter = tf.lite.TFLiteConverter.from_keras_model(model)
+    converter.optimizations = [tf.lite.Optimize.DEFAULT]
+
+    tflite_model = converter.convert()
+
+    with open(tflite_model_path, "wb") as f:
+        f.write(tflite_model)
+    
+    print(f"Modelo otimizado salvo: {tflite_model_path}")
+
+    h5_size = os.path.getsize(model_path) / 1024
+    tflite_size = os.path.getsize(tflite_model_path) / 1024
+    print(f"Tamanho original: {h5_size:.2f} KB")
+    print(f"Tamanho otimizado: {tflite_size:.2f} KB")
+
+if __name__ == "__main__":
+    main()
--- train_model.py
+++ train_model.py
@@ -1,5 +1,41 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+import os
 
-#insira seu código aqui
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+def main():
+    (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+    
+    x_train = x_train.astype("float32") / 255.0
+    x_test = x_test.astype("float32") / 255.0
+    x_train = tf.expand_dims(x_train, -1)
+    x_test = tf.expand_dims(x_test, -1)
+
+    model = keras.Sequential([
+        keras.Input(shape=(28, 28, 1)),
+        layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Flatten(),
+        layers.Dense(64, activation="relu"),
+        layers.Dense(10, activation="softmax")
+    ])
+
+    model.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+
+    model.fit(x_train, y_train, batch_size=128, epochs=5, validation_split=0.1)
+
+
+    test_loss, test_acc = model.evaluate(x_test, y_test, verbose=0)
+    print(f"\nAcuracia: {test_acc:.4f}")
+
+    model_path = "model.h5"
+    model.save(model_path)
+    print(f"Modelo salvo: {model_path}")
+
+if __name__ == "__main__":
+    main()
